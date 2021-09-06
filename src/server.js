140100f1edb50f93edd76232e1a7dd496075a545
Deividy <deividyferreira.n@gmail.com>
2021-09-06 19:39:00 -0300

    pause AULA 01

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require ("express")
+const server = express()
+const routes = require("./views/routes")
+
+server.set("view engine", "ejs")
+
+//habilitar arquivos statics
+server.use(express.static("public"))
+
+
+
+server.use (routes)
+
+server.listen(3000, () => console.log("rodando"))
